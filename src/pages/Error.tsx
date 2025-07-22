017f4169fd715475befd1b407c81a0a586fa9e43
Ashish Rawat <144007425+Ashish252003@users.noreply.github.com>
2025-07-22 20:53:30 +0530

    Delete src directory

--- src/pages/Error.tsx
+++
@@ -1,9 +0,0 @@
- 
-
-const ErrorPage = () => {
-  return (
-    <h1>Page Not Found</h1>
-  )
-}
-
-export default ErrorPage
--- src/pages/Invoice/index.tsx
+++
@@ -1,8 +0,0 @@
- 
-const Invoice = () => {
-  return (
-    <div>Invoice</div>
-  )
-}
-
-export default Invoice
